76dcabf3fb3c372492cf59a41f6cf31de9090fc6
Jen Chow <jennego@gmail.com>
2017-11-18 14:40:24 -0800

    Views for sign in, new user and welcome/logged out page Add bootstrap and jquery

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,7 @@ class User < ApplicationRecord
 	has_many :teams, through: :user_teams
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
-
-	validates :first_name, :last_name, presence: true, if: :is_approved?
+	validates :email, presence: true, uniqueness: true, format: validates :first_name, :last_name, presence: true, if: :is_approved?
 
 	def full_name
 		"#{first_name} #{last_name}"
