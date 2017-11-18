71ad3da3cae4c68ebce6d1ad9432141a9d735af9
Ginny <ginnykloos@gmail.com>
2017-11-18 13:07:24 -0800

    Changes to user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
+
     def new
     end
+    
     def create
         user = User.find_by(email: sessions_params[:email])
         if user && user.authenticate(sessions_params[:password])
