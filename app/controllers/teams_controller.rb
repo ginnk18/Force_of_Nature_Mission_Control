2c14451763c4061721386255c7f6cd3d7412c06a
Ginny <ginnykloos@gmail.com>
2017-11-19 12:07:22 -0800

    Add team show page for current users teams and links to associated google drive docs.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,14 @@
 class TeamsController < ApplicationController
 
 	before_action :authorize_user!
+	before_action :find_team, only: [:show]
 
 	def index
 		@operational_teams = Team.where(team_category: '2').order(created_at: :desc)
 		@regional_teams = Team.where(team_category: '1').order(created_at: :desc)
+	end
+
+	def show
 	end
 
 
@@ -12,14 +16,18 @@ class TeamsController < ApplicationController
 
 	def authorize_user!
 		if current_user
-			unless current_user.user_category.name == 'Admin'
+			unless current_user.user_category.name == 'Team Lead' || current_user.user_category.name == 'Admin'
 				flash[:alert] = 'Access denied sucka!'
 				redirect_to root_path
 			end
 		else
-			flash[:alert] = 'Access denied.'
-			redirect_to root_path
+			flash[:alert] = 'Please sign in first.'
+			redirect_to new_session_path
 		end
+	end
+
+	def find_team
+		@team = Team.find params[:id]
 	end
 
 end
