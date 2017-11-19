cb9bf8d5058ce04d87bf63c347539e6437bb411c
Jen Chow <jennego@gmail.com>
2017-11-18 20:57:19 -0800

    Added fixed-menu, menu to transparent and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,15 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require_tree .
+
+(function($) {
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 450) {
+                $('.navbar').css("background-color", "blue");
+            } else {
+                $('.navbar').css("background-color", "transparent");
+            }
+        });
+    });
+})(jQuery);
