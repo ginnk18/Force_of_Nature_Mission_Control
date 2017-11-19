c5bdd46c4d41f5f3729c09fe8fe0bb8fdfc4fb74
Ginny <ginnykloos@gmail.com>
2017-11-18 22:45:41 -0800

    Resolve more conflicts and test views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
-  root 'events#new'
+  root 'welcome#index'
+  # root 'events#new'
   resources :welcome, only: [:index]
   resource :session, only:[:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :events, only: [:index, :show, :create, :update, :destroy] do
+  resources :events, only: [:index, :new, :show, :create, :update, :destroy] do
      get('newsignup', to: 'eventsignup#new', as: :neweventsignup)
      post('newsignup', to: 'eventsignup#create') 
   end
