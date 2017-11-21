e167cc9ea76891853218b79ed5215fe690a2487f
Ginny <ginnykloos@gmail.com>
2017-11-21 08:45:53 -0800

    Take Brandons email out of mailers.

--- app/mailers/events_mailer.rb
+++ app/mailers/events_mailer.rb
@@ -3,6 +3,6 @@ class EventsMailer < ApplicationMailer
 def notify_event_creator(event)
   @event = event
   @event_owner = @event.creator_id
-  mail(to: 'Brandoneverell@hotmail.com', subject: 'Your created event ')
+  mail(to: 'thissectionclosedcc@gmail.com', subject: 'Your created event ')
   end
 end
