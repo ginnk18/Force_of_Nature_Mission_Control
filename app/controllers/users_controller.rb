9255873cee9ccf02e5266612ddff006b1a575e02
Ginny <ginnykloos@gmail.com>
2017-11-20 16:58:18 -0800

    Fix users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!, except: [:new, :create]  
-  before_action :find_user
+  before_action :authenticate_user!, except: [:new, :create]  
+  before_action :find_user, only: [:edit, :update]
   before_action :authorize_user!, except: [:new, :create]
   
   def new
