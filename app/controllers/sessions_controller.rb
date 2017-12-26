2d07b6e3429d8f5c542b2d68c09ccc97f992dd23
Naveed Raza <naveedraza@gmail.com>
2017-12-25 17:58:57 -0800

    Fixed AM PM and Current Date

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,14 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: sessions_params[:email])
+        
         if user && user.authenticate(sessions_params[:password])
             session[:user_id] = user.id
             flash[:notice] = 'Welcome! Please check-out new events in events calendar'
             redirect_to root_path
         else
             flash.now[:alert] = 'Something went wrong, please try again!'
-            render :new            
+            render :new
         end
     end
 
@@ -24,6 +25,6 @@ class SessionsController < ApplicationController
 
     def sessions_params
         params.require(:session).permit(:email, :password)
-    end 
+    end
 
 end
