7699dd8f7e6808bc1035faa81633992c2bd644ac
Ginny <ginnykloos@gmail.com>
2018-01-02 14:12:00 -0700

    Fix issues with different show pages that did not merge properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226191121) do
+ActiveRecord::Schema.define(version: 20171228023320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
