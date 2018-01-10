8e79607e917c6665745b16b12e7ae59957c5c871
Ginny <ginnykloos@gmail.com>
2018-01-10 12:57:49 -0700

    Finish guest list with ability to check off when they have been contacted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105190752) do
+ActiveRecord::Schema.define(version: 20180110192626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20180105190752) do
     t.string "phone_number"
     t.text "additional_info"
     t.boolean "previous_volunteer", default: false
+    t.boolean "contacted", default: false
     t.index ["user_category_id"], name: "index_users_on_user_category_id"
   end
 
