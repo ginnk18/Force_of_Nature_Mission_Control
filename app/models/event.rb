3da9d1fb895044c91cbd90a5326c29ec59043313
Ginny <ginnykloos@gmail.com>
2018-01-15 17:31:22 -0700

    Fix event creation bug. Take away leads from events since they just have canvas and data captains.

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,7 +10,8 @@ class Event < ApplicationRecord
 	#each event has one creator, which holds a foreign key to a User
 	belongs_to :creator, class_name: "User"
 	#each event has one lead, which holds a foreign key to a User
-	belongs_to :lead, class_name: "User"
+	# belongs_to :lead, class_name: "User"
+	
 	belongs_to :data_captain, :foreign_key => 'data_captain_id',class_name: "User"
 	belongs_to :canvas_captain, :foreign_key => 'canvas_captain_id', class_name: "User"
 	# belongs_to :data_captain, class_name: "User"
