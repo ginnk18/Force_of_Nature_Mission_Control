a788b945e65d41c1062360ff63539fb0b6ec62a9
Jen Chow <jennego@gmail.com>
2018-01-22 22:18:49 -0800

    Swapped Gmail with SendGrid. Contact me for app key/sendgrid sign-in info if needed.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
+  address: 'smtp.sendgrid.net',
   port: '587',
   enable_starttls_auto: true,
   authentication: :plain,
-  user_name: ENV['EMAIL_USER_NAME'],
-  password: ENV['EMAIL_PASSWORD']
+  user_name: ENV['SENDGRID_USER_NAME'],
+  password: ENV['SENDGRID_PASSWORD']
 }
