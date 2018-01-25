7f135ddb425d463ab9f0ddfc2690af2cf420f8a9
Ginny <ginnykloos@gmail.com>
2018-01-24 21:45:21 -0700

    style mailers

--- app/mailers/event_sign_up_mailer.rb
+++ app/mailers/event_sign_up_mailer.rb
@@ -4,7 +4,7 @@ def event_sign_up(event, user)
 @event = event
 @user = user
 @email = @user.email
-mail(to: @email, subject: 'You signed up with Force of Nature')
+mail(to: @email, subject: "You signed up for #{@event.name} with Force of Nature")
 end
 
 end
